346a30e7b4de82c6143f5713a613b0ea8f542843
Ali-907 <24-0172@student.gutech.edu.om>
2026-05-12 18:47:38 +0400

    ThreatDetector.cpp completed

--- ThreatDetector.cpp
+++ ThreatDetector.cpp
@@ -1,1 +1,10 @@
+#include "ThreatDetector.h"
+using namespace std;
 
+ThreatDetector::ThreatDetector(const string& name) {
+    detectorName = name;
+}
+ThreatDetector::~ThreatDetector(){}
+string ThreatDetector::getDetectorName() const {
+    return detectorName;
+}
