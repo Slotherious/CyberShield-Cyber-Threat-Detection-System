cbc63c9c366556b7cc3c432884d72e1ce80ffbe8
Ali-907 <24-0172@student.gutech.edu.om>
2026-05-12 19:14:06 +0400

    comments added

--- ThreatDetector.h
+++ ThreatDetector.h
@@ -3,7 +3,6 @@
 
 #include <string>
 #include <vector>
-
 using namespace std;
 
 class LogEntry;
@@ -12,10 +11,14 @@ class ThreatDetector {
 protected:
     string detectorName;
 public:
+    //ThreatDetector constructor
     ThreatDetector(const string& name);
+
+    //ThreatDetector Destructor after the threat is detected
     virtual ~ThreatDetector();
     virtual vector<Alert> detect(const vector<LogEntry>& logs) = 0;
+
+    //Which detector to use
     string getDetectorName() const;
 };
-
 #endif
